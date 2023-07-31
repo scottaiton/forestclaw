154b51cb5c6ef1a31870656d33dc0c291669f1ff
Scott Aiton <scott@aiton.net>
2023-07-31 16:43:13 -0600

    rename fclaw2d_farraybox to fclaw_farraybox

--- src/fclaw_farraybox.hpp
+++ src/fclaw_farraybox.hpp
@@ -26,7 +26,50 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef FCLAW2D_FARRAYBOX_H
 #define FCLAW2D_FARRAYBOX_H
 
-// this header is kept for backwards compatability
-#include <fclaw_farraybox.hpp>
+#include <vector>
+
+void fclaw_farraybox_set_to_nan(double& f);
+
+class Box
+{
+public:
+    Box() = default;
+    Box(const int ll[], const int ur[], const int box_dim);
+    int smallEnd(int idir) const;
+    int bigEnd(int idir) const;
+    int boxDim() const;
+
+private:
+    int m_box_dim = 0;
+    std::vector<int> m_ll;
+    std::vector<int> m_ur;
+};
+
+class FArrayBox
+{
+public:
+
+    FArrayBox();
+    FArrayBox(const FArrayBox& A);
+    ~FArrayBox();
+    void define(const Box& a_box, int a_fields);
+    double* dataPtr();
+    Box box();
+    int fields();
+    void set_to_value(double &value);
+    void set_to_nan();
+    void set_to_big_number();
+    int size();
+    void operator=(const FArrayBox& fbox);
+    void copyToMemory(double *data);
+    void copyFromMemory(double *data);
+private:
+    double *m_data;
+    int m_size;
+    Box m_box;
+    int m_fields;
+    void set_dataPtr(int size);
+
+};
 
 #endif
