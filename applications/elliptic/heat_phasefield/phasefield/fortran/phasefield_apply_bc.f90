1976d17d55e380fe51be4a3a12ff31c5a30b0ff7
donnaaboise <donna.calhoun@gmail.com>
2024-07-21 16:40:12 -0700

    (elliptic/heat_phasefield) Fix compiler warnings

--- applications/elliptic/heat_phasefield/phasefield/fortran/phasefield_apply_bc.f90
+++ applications/elliptic/heat_phasefield/phasefield/fortran/phasefield_apply_bc.f90
@@ -67,6 +67,9 @@ subroutine phasefield_fort_apply_bc(blockno, mx, my,mbc,mfields,xlower,ylower, &
                 elseif (bctype(iface) .eq. 2) then
                     a = 0
                     b = 1
+                else
+                    write(6,*) 'phasefield_apply_bc : bctype(iface) not valid'
+                    stop
                 endif
 
                 if (idir == 0) then
@@ -104,6 +107,9 @@ subroutine phasefield_fort_apply_bc(blockno, mx, my,mbc,mfields,xlower,ylower, &
                         ic = mx  !! cell center
                         i1 = mx+1
                         ig = mx+1
+                    else
+                        write(6,*) 'phasefield_appy_bc: ic, i1, ig not initialized'
+                        stop
                     endif
                     !! location at interface
                     x = xlower + (i1 - 1)*dx    
@@ -130,6 +136,9 @@ subroutine phasefield_fort_apply_bc(blockno, mx, my,mbc,mfields,xlower,ylower, &
                         jc = my
                         j1 = my+1
                         jg = my+1
+                    else
+                        write(6,*) 'phasefield_appy_bc: jc, j1, jg not initialized'
+                        stop
                     endif
                     !! location at interface
                     y = ylower + (j1 - 1)*dy
