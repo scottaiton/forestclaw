cecb7cff22fdad8eb8b0920efc0cd20dac28f585
donnaaboise <donna.calhoun@gmail.com>
2024-07-22 12:57:08 -0700

    (elliptic/heat) Fix compiler warnings

--- applications/elliptic/heat/fortran/heat_eval_bc.f90
+++ applications/elliptic/heat/fortran/heat_eval_bc.f90
@@ -24,6 +24,9 @@ double precision function heat_fort_eval_bc(iface,t,x,y)
     elseif (bctype(iface) .eq. 2) then
         a = 0
         b = 1
+    else
+        write(6,*) 'heat_eval_bc : Invalid bctype'
+        stop
     endif
 
     heat_fort_eval_bc = a*q + b*qn
