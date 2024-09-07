c17b4715706119891ac17d6d51bbed111f422347
donnaaboise <donna.calhoun@gmail.com>
2024-09-07 17:02:22 -0600

    (applications/clawpack) Update list of apps to add periodic3d example

--- applications/clawpack/claw3.cmake
+++ applications/clawpack/claw3.cmake
@@ -3,15 +3,38 @@
 # ----------------------------------
 
 
-# Scalar advection
+# -----------------------
+# Scalar advection (3dx)
+# -----------------------
+
+# swirl : Uses aux array for variable velocity field
 add_subdirectory(clawpack/advection/3dx/swirl)
-add_subdirectory(clawpack/advection/3d/swirl)
+
+# filament : Uses aux array for variable velocity field
 add_subdirectory(clawpack/advection/3dx/filament)
+
+# sphere : Uses aux array for variable velocity field
 add_subdirectory(clawpack/advection/3dx/sphere)
+
+# disk : Uses aux array for variable velocity field
 add_subdirectory(clawpack/advection/3dx/disk)
+
+# latlong : Uses aux array for variable velocity field
 add_subdirectory(clawpack/advection/3dx/latlong)
 
-# Euler equations
+# -----------------------
+# Scalar advection (3d)
+# -----------------------
+
+# swirl : Uses aux array for variable velocity field
+add_subdirectory(clawpack/advection/3d/swirl)
+
+# periodic : Uses constant velocity field
+add_subdirectory(clawpack/advection/3d/periodic)
+
+# -----------------------
+# Euler equations (3dx)
+# -----------------------
 add_subdirectory(clawpack/euler/3dx/overpressure)
 
 
