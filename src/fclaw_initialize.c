6285c84229b8f1c453927edf4e7651545902f126
Scott Aiton <scott@aiton.net>
2024-10-18 22:57:01 -0600

    use partition_mode_enum to set domain partitioning

--- src/fclaw_initialize.c
+++ src/fclaw_initialize.c
@@ -203,9 +203,16 @@ void fclaw_initialize_domain_flags(fclaw_global_t *glob)
         (glob->domain, fclaw_opt->smooth_refine, fclaw_opt->smooth_level,
          fclaw_opt->coarsen_delay);
 
+    int skip_local = fclaw_opt->regrid_mode == FCLAW_OPTIONS_REGRID_MODE_SKIP_LOCAL || 
+                     fclaw_opt->regrid_mode == FCLAW_OPTIONS_REGRID_MODE_REFINE_AFTER;
+    
+    int skip_refined = fclaw_opt->regrid_mode == FCLAW_OPTIONS_REGRID_MODE_REFINE_AFTER;
+
     /* set partitioning */
     fclaw_domain_set_partitioning (glob->domain,
-                                   fclaw_opt->partition_for_coarsening, 1, 0);
+                                   fclaw_opt->partition_for_coarsening, 
+                                   skip_local,
+                                   skip_refined);
 }
 
 static void
